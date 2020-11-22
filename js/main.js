23195375e872e25f96ccc9da7441f8a2c4a8a660
André Jesus Brito <35274086+AndreJesusBrito@users.noreply.github.com>
2020-11-22 17:28:43 +0000

    feat: show 100 random banners

--- js/main.js
+++ js/main.js
@@ -14,21 +14,43 @@ document.body.append(previewCanvas);
 
 const previewCtx = previewCanvas.getContext("2d");
 
+/**
+ * Maps the banners with they canvas
+ * @type {CanvasRenderingContext2D[]}
+ */
+const bannerCanvasContexts = [];
+
+
+
 loadPatterns().then((patterns) => {
-  const myBanner = new Banner(5, [
-    random(0, 16),
-    random(1, patterns.length - 1),
-    random(0, colors.length - 1),
+  for (let i = 0; i < 100; i++) {
+    const myBanner = new Banner(5, [
+      random(0, 16),
+      random(1, patterns.length - 1),
+      random(0, colors.length - 1),
 
-    random(0, patterns.length - 1),
-    random(0, colors.length - 1),
+      random(0, patterns.length - 1),
+      random(0, colors.length - 1),
 
-    random(0, patterns.length - 1),
-    random(0, colors.length - 1),
+      random(0, patterns.length - 1),
+      random(0, colors.length - 1),
 
-    random(0, patterns.length - 1),
-    random(0, colors.length - 1),
-  ]);
+      random(0, patterns.length - 1),
+      random(0, colors.length - 1),
+    ]);
 
-  myBanner.render(drawPattern, previewCtx, colors, patterns);
+    const bannerCanvas = document.createElement("canvas");
+    bannerCanvas.width = 20;
+    bannerCanvas.height = 40;
+
+    const bannerContext = bannerCanvas.getContext('2d');
+    document.body.append(bannerCanvas);
+
+
+    myBanner.render(drawPattern, bannerContext, colors, patterns);
+
+    bannerCanvasContexts.push(bannerContext);
+
+  }
+
 });
